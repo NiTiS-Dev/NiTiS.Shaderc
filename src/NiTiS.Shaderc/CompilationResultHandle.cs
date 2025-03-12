49a16357c426c96b11bd4969f1df260c4bda86e0
NiTiSon <NiTiSon@users.noreply.github.com>
2025-03-13 02:12:55 +0300

    Add more methods

--- src/NiTiS.Shaderc/CompilationResultHandle.cs
+++ src/NiTiS.Shaderc/CompilationResultHandle.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace NiTiS.Shaderc;
 
 /// <summary>
 /// Compilation result handle.
 /// </summary>
-public readonly struct CompilationResultHandle
+public readonly struct CompilationResultHandle : IDisposable
 {
 	/// <summary>
 	/// Native result handle.
@@ -15,5 +16,52 @@ public readonly struct CompilationResultHandle
 	internal CompilationResultHandle(nint handle)
 	{
 		Handle = handle;
+	}
+
+	/// <summary>
+	/// Status of compilation.
+	/// </summary>
+	public CompilationStatus Status
+	{
+		get
+		{
+			return shaderc_result_get_compilation_status(this);
+		}
+	}
+
+	/// <summary>
+	/// Length of compilation output.
+	/// </summary>
+	public nuint Length
+	{
+		get
+		{
+			return shaderc_result_get_length(this);
+		}
+	}
+
+	/// <inheritdoc/>
+	public void Dispose()
+	{
+		shaderc_result_release(this);
+	}
+
+	/// <summary>
+	/// Copy result output into provided span.
+	/// </summary>
+	/// <param name="output">The span to store output.</param>
+	/// <returns>Amount of copied bytes.</returns>
+	public unsafe nuint CopyTo(Span<byte> output)
+	{
+		byte* src = shaderc_result_get_bytes(this);
+
+		nuint length = (ulong)output.Length > Length ? (nuint)output.Length : Length;
+
+		fixed (byte* pOutput = output)
+		{
+			Unsafe.CopyBlockUnaligned(pOutput, src, (uint)length);
+		}
+
+		return length;
 	}
 }
--- src/NiTiS.Shaderc/CompilerHandle.cs
+++ src/NiTiS.Shaderc/CompilerHandle.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Diagnostics;
 using System;
 
 namespace NiTiS.Shaderc;
@@ -5,7 +6,7 @@ namespace NiTiS.Shaderc;
 /// <summary>
 /// Compiler handle.
 /// </summary>
-public readonly struct CompilerHandle : IDisposable
+public readonly unsafe struct CompilerHandle : IDisposable
 {
 	/// <summary>
 	/// Native compiler handle.
@@ -29,5 +30,20 @@ public readonly struct CompilerHandle : IDisposable
 	public void Dispose()
 	{
 		shaderc_compiler_release(this);
+	}
+
+	/// <summary>
+	/// Compile source shader code into SPIR-V bytecode.
+	/// </summary>
+	/// <param name="utf8Source">Source bytes.</param>
+	/// <param name="sourceLength">Source length.</param>
+	/// <param name="kind">Shader kind.</param>
+	/// <param name="utf8SourceName">Source name bytes (null terminated).</param>
+	/// <param name="utf8EntryPointName">Shader entry point name (null terminated).</param>
+	/// <param name="options">Compilation options</param>
+	/// <returns>Compilation result handle.</returns>
+	public CompilationResultHandle CompileIntoSpv(byte* utf8Source, nuint sourceLength, ShaderKind kind, byte* utf8SourceName, byte* utf8EntryPointName, CompileOptionsHandle options)
+	{
+		return shaderc_compile_into_spv(this, utf8Source, sourceLength, kind, utf8SourceName, utf8EntryPointName, options);
 	}
 }
